aa259b2a86aef5dfda30b662964501a1247f5e36
Goulven Nev <nevilain@gmail.com>
2021-12-23 09:53:11 +0100

    fix plugin styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/*.tsx", 
-    "./src/**/*.tsx"
+    "./src/**/*.tsx",
+    // Temporary solution to keep the my plugin's styles
+    "./node_modules/\\@goulvenc/typescript-table/dist/*.js",
+    "./node_modules/\\@goulvenc/typescript-table/dist/**/*.js"
   ],
   theme: {
     extend: {},
